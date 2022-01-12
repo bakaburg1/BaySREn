d1e2651da01424d0416b5022d40e4334ad079696
Angelo D'Ambrosio <bakaburg1@gmail.com>
2022-01-12 17:52:27 +0100

    ignore secret API data

--- tests/testthat/secrets.R
+++
@@ -1,3 +0,0 @@
-options(baysren.ieee_api_key = 'bb58ndv4xbv535b5779u93ka')
-options(baysren.ncbi_api_key = '5cd65c6d044db651af2bf8e9c78fc54fa409')
-options(baysren.wos_api_key = 'C4O2SHqGc1rf3jmNxqI')
