acdc2630828a20c8f527415c90a32f5f049615fe
Angelo D'Ambrosio <bakaburg1@gmail.com>
2022-01-19 12:51:16 +0100

    test: add deffered cleaning and comment out output testing
    To test outputs first reproducibility needs to be implemented
    
    plus version bump

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -1,16 +1,21 @@
-unlink(list.files(pattern = ".pID", all.files = TRUE))
-unlink("Model_backup.rds")
-unlink("Sessions/Session1/Annotations/", recursive = TRUE)
-unlink("Sessions/Session1/Results/", recursive = TRUE)
-unlink("Sessions/Session1/Samples/", recursive = TRUE)
-unlink("Sessions/Session1/DTM.rds", recursive = TRUE)
-
-print(tryCatch(rJava::.jcheck(silent = TRUE) == 0, error = function(e) FALSE))
-
 oldMem <- getOption("baysren.BartMem")
+
+withr::defer(
+  {
+    options(baysren.BartMem = oldMem)
+    unlink(list.files(pattern = ".pID", all.files = TRUE))
+    unlink("Model_backup.rds")
+    unlink("Sessions/Session1/Annotations/", recursive = TRUE)
+    unlink("Sessions/Session1/Results/", recursive = TRUE)
+    unlink("Sessions/Session1/Samples/", recursive = TRUE)
+    unlink("Sessions/Session1/DTM.rds", recursive = TRUE)
+  },
+  teardown_env()
+)
+
 options(baysren.BartMem = "16")
 
-test_that("annotation file classification doesn't throws errors.", {
+test_that("annotation file classification doesn't throw errors.", {
   set.seed(1482398429)
 
   expect_error(enrich_annotation_file("Session1", pos_mult = 20, n_models = 2, autorun = TRUE, limits = list(
@@ -29,30 +34,29 @@ sessions_files_exist <- test_that("the expected number of files was created.", {
   expect_mapequal(obs, exp)
 })
 
-if (isTRUE(sessions_files_exist)) {
-  test_that("test DTM", {
-    expect_snapshot_file(session_files$DTM)
-  })
-
-  #  There should be three versions of each file type, so no dynamic init here
-  for (i in 1:3) {
-    test_that(paste("Samples file", i, "matches expectation."), {
-      data <- import_data(session_files$Samples[i])[1:20, 1:20] # Limit memory footprint
-      expect_snapshot_value(data, style = "serialize")
-    })
-
-    test_that(paste("Record file", i, "matches expectation."), {
-      data <- import_data(session_files$Results[i])[-2, ] # Remove fields with timestamps
-      expect_snapshot_value(data, style = "deparse")
-    })
-
-    test_that(paste("Annotation file", i, "matches expectation."), {
-      data <- import_data(session_files$Annotations[i]) %>%
-        select(matches("^(Prev_|Rev_|Order)")) %>% # Keep varying fields
-        head(20)
-      expect_snapshot_value(data, style = "serialize")
-    })
-  }
-}
-
-options(baysren.BartMem = oldMem)
+# TODO: renable once reproducible predictions are implemented
+# if (isTRUE(sessions_files_exist)) {
+#   test_that("DTM matches expectation.", {
+#     expect_snapshot_file(session_files$DTM)
+#   })
+#
+#   #  There should be three versions of each file type, so no dynamic init here
+#   for (i in 1:3) {
+#     test_that(paste("Samples file", i, "matches expectation."), {
+#       data <- import_data(session_files$Samples[i])[1:20, 1:20] # Limit memory footprint
+#       expect_snapshot_value(data, style = "serialize")
+#     })
+#
+#     test_that(paste("Record file", i, "matches expectation."), {
+#       data <- import_data(session_files$Results[i])[-2, ] # Remove fields with timestamps
+#       expect_snapshot_value(data, style = "deparse")
+#     })
+#
+#     test_that(paste("Annotation file", i, "matches expectation."), {
+#       data <- import_data(session_files$Annotations[i]) %>%
+#         select(matches("^(Prev_|Rev_|Order)")) %>% # Keep varying fields
+#         head(20)
+#       expect_snapshot_value(data, style = "serialize")
+#     })
+#   }
+# }
